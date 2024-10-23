78000900498d5bcf5bba7a71b290f43b75f2bf27
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-10-23 22:31:06 +0300

    update App.java and add Url.java and BaseRepository.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,18 +1,42 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
 
 @Slf4j
 public class App {
+    public static Javalin getApp() throws IOException, SQLException {
+        // Создание БД Н2 в памяти
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDataBaseUrl());
+        var dataSource = new HikariDataSource(hikariConfig);
 
-    private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
-        return Integer.parseInt(port);
-    }
-    public static Javalin getApp() {
+        //Получение пути до файла src/main/resources/schema.sql,
+        // содержащий sql-запросы на создание таблиц в БД
+        var url = App.class.getClassLoader().getResourceAsStream("schema.sql");
+        var sql = new BufferedReader(new InputStreamReader(url))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        log.info(sql);
+
+        // Установка соединения и инициализация БД через стейтмент
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
 
         Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
@@ -26,7 +50,18 @@ public class App {
         return app;
     }
 
-    public static void main(String[] args) {
+    private static String getDataBaseUrl() {
+        String url = System.getenv().getOrDefault("DATABASE_URL",
+                "jdbc:h2:mem:project");
+        return url;
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
         Javalin app = getApp();
         app.start(getPort());
     }
