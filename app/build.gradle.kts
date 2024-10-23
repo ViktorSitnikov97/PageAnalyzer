63e187ac60db0c6856d085c8de4a1fa7adf2ee82
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-10-23 16:26:18 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,10 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
+    checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
@@ -16,4 +21,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
