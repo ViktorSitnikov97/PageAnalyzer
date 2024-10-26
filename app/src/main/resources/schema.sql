5b7c8b0e763190f9eafd1cbeddac16558a526716
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-10-26 16:49:51 +0300

    update schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE urls (
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE url_checks (
+--    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    url_id BIGINT REFERENCES urls(id) ON DELETE CASCADE,
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP NOT NULL
+);
