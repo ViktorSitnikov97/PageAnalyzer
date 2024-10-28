b3622b6a7bf3ca323a02c5061cf8fd3ba126b679
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-10-28 19:39:12 +0300

    update RootController.java

--- app/src/main/java/hexlet/code/controllers/RootController.java
+++ app/src/main/java/hexlet/code/controllers/RootController.java
@@ -1,9 +1,15 @@
 package hexlet.code.controllers;
 
+import hexlet.code.dto.BasePage;
 import io.javalin.http.Context;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class RootController {
     public static void indexRoot(Context ctx) {
-        ctx.render("index.jte");
+        BasePage page = new BasePage();
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flashType"));
+        ctx.render("index.jte", model("page", page));
     }
 }
