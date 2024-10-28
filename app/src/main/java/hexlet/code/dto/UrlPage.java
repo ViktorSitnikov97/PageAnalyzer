006a8846dc6f03e6faaf4686c11379c2e65c91c3
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-10-28 19:40:45 +0300

    refactoring UrlPage.java and UrlsPage.java

--- app/src/main/java/hexlet/code/dto/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/UrlPage.java
@@ -2,14 +2,18 @@ package hexlet.code.dto;
 
 import hexlet.code.model.Url;
 import hexlet.code.model.UrlCheck;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+
 @Getter
-@AllArgsConstructor
-public class UrlPage {
+@Setter
+public class UrlPage extends BasePage {
     private Url url;
-    private List<UrlCheck> urlChecks;
+    private List<UrlCheck> checks;
+    public UrlPage(Url url) {
+        this.url = url;
+    }
 }
